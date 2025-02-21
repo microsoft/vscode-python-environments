efc0d626d0933094c591d170f891f057360d378d
Karthik Nadig <kanadig@microsoft.com>
2025-02-20 18:06:32 -0800

    feat: add activation using shell startup

--- src/features/terminal/startup/startupProvider.ts
+++ src/features/terminal/startup/startupProvider.ts
@@ -1,9 +1,11 @@
-import { GlobalEnvironmentVariableCollection, Uri } from 'vscode';
+import { EnvironmentVariableCollection } from 'vscode';
+import { PythonEnvironment } from '../../../api';
 
 export interface ShellStartupProvider {
     isSetup(): Promise<boolean>;
     setupScripts(): Promise<void>;
-    removeScripts(): Promise<void>;
-    updateEnvVariables(global: GlobalEnvironmentVariableCollection, scope?: Uri): Promise<void>;
-    removeEnvVariables(global: GlobalEnvironmentVariableCollection, scope?: Uri): Promise<void>;
+    teardownScripts(): Promise<void>;
+    updateEnvVariables(envVars: EnvironmentVariableCollection, env: PythonEnvironment): Promise<void>;
+    removeEnvVariables(envVars: EnvironmentVariableCollection): Promise<void>;
+    getEnvVariables(env: PythonEnvironment): Promise<Map<string, string> | undefined>;
 }
