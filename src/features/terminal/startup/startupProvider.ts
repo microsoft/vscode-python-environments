be18bb415bc0224a101064e70856d4797c2a22c7
Karthik Nadig <kanadig@microsoft.com>
2025-03-19 20:03:32 -0700

    feat: Add fish and some fixes

--- src/features/terminal/startup/startupProvider.ts
+++ src/features/terminal/startup/startupProvider.ts
@@ -1,11 +1,23 @@
 import { EnvironmentVariableCollection } from 'vscode';
 import { PythonEnvironment } from '../../../api';
 
+export enum ShellSetupState {
+    NotSetup,
+    Setup,
+    NotInstalled,
+}
+
+export enum ShellScriptEditState {
+    NotEdited,
+    Edited,
+    NotInstalled,
+}
+
 export interface ShellStartupProvider {
     name: string;
-    isSetup(): Promise<boolean>;
-    setupScripts(): Promise<boolean>;
-    teardownScripts(): Promise<boolean>;
+    isSetup(): Promise<ShellSetupState>;
+    setupScripts(): Promise<ShellScriptEditState>;
+    teardownScripts(): Promise<ShellScriptEditState>;
     updateEnvVariables(envVars: EnvironmentVariableCollection, env: PythonEnvironment): Promise<void>;
     removeEnvVariables(envVars: EnvironmentVariableCollection): Promise<void>;
     getEnvVariables(env?: PythonEnvironment): Promise<Map<string, string | undefined> | undefined>;
