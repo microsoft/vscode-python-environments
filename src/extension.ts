330c9d473293d96982541e8d3ae3ba1fab421c41
Eleanor Boyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-04-16 15:11:56 -0700

    Adding menu items (#310)

--- src/extension.ts
+++ src/extension.ts
@@ -19,6 +19,8 @@ import {
 import { GetEnvironmentInfoTool, InstallPackageTool } from './features/copilotTools';
 import { AutoFindProjects } from './features/creators/autoFindProjects';
 import { ExistingProjects } from './features/creators/existingProjects';
+import { NewPackageProject } from './features/creators/newPackageProject';
+import { NewScriptProject } from './features/creators/newScriptProject';
 import { ProjectCreatorsImpl } from './features/creators/projectCreators';
 import {
     addPythonProject,
@@ -110,6 +112,8 @@ export async function activate(context: ExtensionContext): Promise<PythonEnviron
         projectCreators,
         projectCreators.registerPythonProjectCreator(new ExistingProjects(projectManager)),
         projectCreators.registerPythonProjectCreator(new AutoFindProjects(projectManager)),
+        projectCreators.registerPythonProjectCreator(new NewPackageProject()),
+        projectCreators.registerPythonProjectCreator(new NewScriptProject()),
     );
 
     setPythonApi(envManagers, projectManager, projectCreators, terminalManager, envVarManager);
