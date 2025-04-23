98e80c2def9fbb0feb9100efed13fbd7b8caf1c5
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-04-23 15:16:50 -0700

    cleanup

--- src/features/creators/newPackageProject.ts
+++ src/features/creators/newPackageProject.ts
@@ -7,11 +7,11 @@ import {
     promptForCopilotInstructions,
     isCopilotInstalled,
     quickCreateNewVenv,
-    removeCopilotInstructions,
     replaceInFilesAndNames,
-    replaceInFile,
+    manageCopilotInstructionsFile,
+    manageLaunchJsonFile,
 } from './creationHelpers';
-import { EXTENSION_ROOT_DIR } from '../../common/constants';
+import { NEW_PROJECT_TEMPLATES_FOLDER } from '../../common/constants';
 import { EnvironmentManagers } from '../../internal.api';
 import { showInputBoxWithButtons } from '../../common/window.apis';
 
@@ -23,13 +23,16 @@ export class NewPackageProject implements PythonProjectCreator {
 
     constructor(private readonly envManagers: EnvironmentManagers) {}
 
-    async create(_options?: PythonProjectCreatorOptions): Promise<PythonProject | undefined> {
-        // Prompt for package name (TODO: this doesn't make sense if the _options is already being passed in )
-        const packageName = await showInputBoxWithButtons({
-            prompt: 'What is the name of the package? (e.g. my_package)',
-            ignoreFocusOut: true,
-            showBackButton: true,
-        });
+    async create(options?: PythonProjectCreatorOptions): Promise<PythonProject | undefined> {
+        // Prompt for package name if not provided
+        let packageName = options?.name;
+        if (!packageName) {
+            packageName = await showInputBoxWithButtons({
+                prompt: 'What is the name of the package? (e.g. my_package)',
+                ignoreFocusOut: true,
+                showBackButton: true,
+            });
+        }
         if (!packageName) {
             return undefined;
         }
@@ -55,102 +58,73 @@ export class NewPackageProject implements PythonProjectCreator {
         );
 
         // 1. Copy template folder
-        const templateFolder = path.join(
-            EXTENSION_ROOT_DIR,
-            'src',
-            'features',
-            'creators',
-            'templates',
-            'newPackageTemplate',
-        );
-        if (!(await fs.pathExists(templateFolder))) {
-            window.showErrorMessage('Template folder does not exist.');
-            return undefined;
-            // might need another check or error handling here
-        }
-        const workspaceFolders = workspace.workspaceFolders;
-        if (!workspaceFolders || workspaceFolders.length === 0) {
-            window.showErrorMessage('No workspace folder is open.');
+        const newPackageTemplateFolder = path.join(NEW_PROJECT_TEMPLATES_FOLDER, 'newPackageTemplate');
+        if (!(await fs.pathExists(newPackageTemplateFolder))) {
+            window.showErrorMessage('Template folder does not exist, aborting creation.');
             return undefined;
         }
-        const destRoot = workspaceFolders[0].uri.fsPath; // this doesn't seem right...
-        // Check if the destination folder already exists
-        const destFolder = path.join(destRoot, `${packageName}_project`);
-        if (await fs.pathExists(destFolder)) {
-            window.showErrorMessage('A project folder by that name already exists, aborting.');
-            return undefined;
-        }
-        await fs.copy(templateFolder, destFolder);
 
-        // custom instructions
-        const instructionsTextPath = path.join(
-            EXTENSION_ROOT_DIR,
-            'src',
-            'features',
-            'creators',
-            'templates',
-            'copilot-instructions-text',
-            'package-copilot-instructions.md',
-        );
-        const instructionsText = `\n \n` + (await fs.readFile(instructionsTextPath, 'utf-8'));
-
-        // check to see if .github folder exists
-        const githubFolderPath = path.join(destRoot, '.github');
-        const customInstructionsPath = path.join(githubFolderPath, 'copilot-instructions.md');
-        const ghFolder = await fs.pathExists(githubFolderPath);
-        if (ghFolder) {
-            const customInstructions = await fs.pathExists(customInstructionsPath);
-            if (customInstructions) {
-                // Append to the existing file
-                await fs.appendFile(customInstructionsPath, instructionsText);
-            } else {
-                // Create the file if it doesn't exist
-                await fs.writeFile(customInstructionsPath, instructionsText);
+        // Check if the destination folder is provided, otherwise use the first workspace folder
+        let destRoot = options?.uri?.fsPath;
+        if (!destRoot) {
+            const workspaceFolders = workspace.workspaceFolders;
+            if (!workspaceFolders || workspaceFolders.length === 0) {
+                window.showErrorMessage('No workspace folder is open or provided, aborting creation.');
+                return undefined;
             }
-        } else {
-            // Create the .github folder and the file
-            await fs.mkdir(githubFolderPath);
-            await fs.writeFile(customInstructionsPath, instructionsText);
+            destRoot = workspaceFolders[0].uri.fsPath;
         }
 
-        // 2. Replace <package_name> in all files and file/folder names using helper
-        await replaceInFilesAndNames(destFolder, 'package_name', packageName);
-
-        // 3. Remove Copilot instructions folder if needed
-        if (!createCopilotInstructions) {
-            await removeCopilotInstructions(destFolder);
+        // Check if the destination folder already exists
+        const projectDestinationFolder = path.join(destRoot, `${packageName}_project`);
+        if (await fs.pathExists(projectDestinationFolder)) {
+            window.showErrorMessage(
+                'A project folder by that name already exists, aborting creation. Please retry with a unique package name given your workspace.',
+            );
+            return undefined;
         }
+        await fs.copy(newPackageTemplateFolder, projectDestinationFolder);
+
+        // 2. Replace 'package_name' in all files and file/folder names using a helper
+        await replaceInFilesAndNames(projectDestinationFolder, 'package_name', packageName);
 
         // 4. Create virtual environment if requested
         if (createVenv) {
-            await quickCreateNewVenv(this.envManagers, destFolder);
+            await quickCreateNewVenv(this.envManagers, projectDestinationFolder);
         }
 
         // 5. Get the Python environment for the destination folder
-        // could be either the one created in step 4 or an existing one
-        const pythonEnvironment = await this.envManagers.getEnvironment(Uri.parse(destFolder));
+        // could be either the one created in an early step or an existing one
+        const pythonEnvironment = await this.envManagers.getEnvironment(Uri.parse(projectDestinationFolder));
 
-        // 6. Replace <run_exec> and <activation_command> in README.md
-        // const readmeFilePath = path.join(destFolder, 'README.md');
         if (!pythonEnvironment) {
             window.showErrorMessage('Python environment not found.');
             return undefined;
         }
-        const execInfo = pythonEnvironment.execInfo;
-        if (execInfo.run) {
-            // const { executable, args = [] } = execInfo.run;
-            // const execRunStr = [executable, ...args].join(' ');
-            // TODO: check this as I don't think I need this anymore
-            await replaceInFile(customInstructionsPath, '<package_name>', packageName);
+
+        // add custom github copilot instructions
+        if (createCopilotInstructions) {
+            const packageInstructionsPath = path.join(
+                NEW_PROJECT_TEMPLATES_FOLDER,
+                'copilot-instructions-text',
+                'package-copilot-instructions.md',
+            );
+            await manageCopilotInstructionsFile(destRoot, packageName, packageInstructionsPath);
         }
 
-        // TODO: insert copilot instructions text into the copilot instructions file
-        // TODO: insert configs into existing launch.json file
+        // update launch.json file with config for the package
+        const launchJsonConfig = {
+            name: `Python Package: ${packageName}`,
+            type: 'debugpy',
+            request: 'launch',
+            module: packageName,
+        };
+        await manageLaunchJsonFile(destRoot, JSON.stringify(launchJsonConfig));
 
         // Return a PythonProject OR Uri (if no venv was created)
         return {
             name: packageName,
-            uri: Uri.file(destFolder),
+            uri: Uri.file(projectDestinationFolder),
         };
     }
 }
