69a40ffdf89f3b0668e59fa996d259172909a56c
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-05-13 16:03:33 -0400

    stop autoFindProjects from finding in .venv

--- src/features/creators/autoFindProjects.ts
+++ src/features/creators/autoFindProjects.ts
@@ -57,7 +57,7 @@ export class AutoFindProjects implements PythonProjectCreator {
     constructor(private readonly pm: PythonProjectManager) {}
 
     async create(_options?: PythonProjectCreatorOptions): Promise<PythonProject | PythonProject[] | undefined> {
-        const files = await findFiles('**/{pyproject.toml,setup.py}');
+        const files = await findFiles('**/{pyproject.toml,setup.py}', '**/.venv/**');
         if (!files || files.length === 0) {
             setImmediate(() => {
                 showErrorMessage('No projects found');
