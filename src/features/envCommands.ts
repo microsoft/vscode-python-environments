94badeb514766b48759ec6b1e078475ea9ff248e
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-08-18 15:13:45 -0700

    add resolution attempt

--- src/features/envCommands.ts
+++ src/features/envCommands.ts
@@ -574,8 +574,10 @@ export async function runInTerminalCommand(
         const project = api.getPythonProject(uri);
         const environment = await api.getEnvironment(uri);
         if (environment && project) {
-            const terminal = await tm.getProjectTerminal(project, environment);
-            await runInTerminal(environment, terminal, {
+            const resolvedEnv = await api.resolveEnvironment(environment.environmentPath);
+            const envFinal = resolvedEnv ?? environment;
+            const terminal = await tm.getProjectTerminal(project, envFinal);
+            await runInTerminal(envFinal, terminal, {
                 cwd: project.uri,
                 args: [item.fsPath],
                 show: true,
@@ -594,9 +596,13 @@ export async function runInDedicatedTerminalCommand(
         const uri = item as Uri;
         const project = api.getPythonProject(uri);
         const environment = await api.getEnvironment(uri);
+
+        // add resolution here
         if (environment && project) {
-            const terminal = await tm.getDedicatedTerminal(item, project, environment);
-            await runInTerminal(environment, terminal, {
+            const resolvedEnv = await api.resolveEnvironment(environment.environmentPath);
+            const envFinal = resolvedEnv ?? environment;
+            const terminal = await tm.getDedicatedTerminal(item, project, envFinal);
+            await runInTerminal(envFinal, terminal, {
                 cwd: project.uri,
                 args: [item.fsPath],
                 show: true,
@@ -612,8 +618,10 @@ export async function runAsTaskCommand(item: unknown, api: PythonEnvironmentApi)
         const project = api.getPythonProject(uri);
         const environment = await api.getEnvironment(uri);
         if (environment) {
+            const resolvedEnv = await api.resolveEnvironment(environment.environmentPath);
+            const envFinal = resolvedEnv ?? environment;
             return await runAsTask(
-                environment,
+                envFinal,
                 {
                     project,
                     args: [item.fsPath],
@@ -624,6 +632,7 @@ export async function runAsTaskCommand(item: unknown, api: PythonEnvironmentApi)
             );
         }
     } else if (item === undefined) {
+        // If no context is provided, try to use the active text editor
         const uri = activeTextEditor()?.document.uri;
         if (uri) {
             return runAsTaskCommand(uri, api);
