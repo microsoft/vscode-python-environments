ff91421af3020e40f98086b549817add3246af8a
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-10-26 02:44:50 -0700

    Update src/common/utils/asyncUtils.ts

--- src/common/utils/asyncUtils.ts
+++ src/common/utils/asyncUtils.ts
@@ -1,5 +1,3 @@
 export async function timeout(milliseconds: number): Promise<void> {
     return new Promise<void>((resolve) => setTimeout(resolve, milliseconds));
 }
-
-// TODO: Advanced timeout from core async: https://github.com/microsoft/vscode-python-environments/issues/953
