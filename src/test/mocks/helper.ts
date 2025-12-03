a2b85497957a7fccb207d439d444a420c9ff010c
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-12-02 19:00:18 -0800

    add api tests for packageManager

--- src/test/mocks/helper.ts
+++ src/test/mocks/helper.ts
@@ -1,9 +1,8 @@
- 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as TypeMoq from 'typemoq';
 import { Readable } from 'stream';
+import * as TypeMoq from 'typemoq';
 import * as common from 'typemoq/Common/_all';
 
 export class FakeReadableStream extends Readable {
@@ -11,6 +10,26 @@ export class FakeReadableStream extends Readable {
         // custom reading logic here
         this.push(null); // end the stream
     }
+}
+
+/**
+ * Type helper for accessing the `.then` property on mocks.
+ * Used to prevent TypeMoq from treating mocks as thenables (Promise-like objects).
+ * See: https://github.com/florinn/typemoq/issues/67
+ */
+export type Thenable = { then?: unknown };
+
+/**
+ * Sets up a mock to not be treated as a thenable (Promise-like object).
+ * This is necessary due to a TypeMoq limitation where mocks can be confused with Promises.
+ *
+ * @param mock - The TypeMoq mock to configure
+ * @example
+ * const mock = TypeMoq.Mock.ofType<MyInterface>();
+ * setupNonThenable(mock);
+ */
+export function setupNonThenable<T>(mock: TypeMoq.IMock<T>): void {
+    mock.setup((x) => (x as unknown as Thenable).then).returns(() => undefined);
 }
 
 export function createTypeMoq<T>(
@@ -22,6 +41,6 @@ export function createTypeMoq<T>(
     // Use typemoqs for those things that are resolved as promises. mockito doesn't allow nesting of mocks. ES6 Proxy class
     // is the problem. We still need to make it thenable though. See this issue: https://github.com/florinn/typemoq/issues/67
     const result = TypeMoq.Mock.ofType<T>(targetCtor, behavior, shouldOverrideTarget, ...targetCtorArgs);
-    result.setup((x: any) => x.then).returns(() => undefined);
+    setupNonThenable(result);
     return result;
 }
