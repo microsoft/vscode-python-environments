a3f0608a39c7527d2b8ce7dcccc53599767b9da8
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2026-02-03 12:27:11 -0800

    Use XDF_CONFIG_HOME for fish config when available (#1164)
    Fixes #1158

---
+++ src/test/features/terminal/shells/fish/fishStartup.unit.test.ts
@@ -0,0 +1,41 @@
+import * as assert from 'assert';
+import * as os from 'os';
+import * as path from 'path';
+import * as sinon from 'sinon';
+
+import { getFishProfile } from '../../../../../features/terminal/shells/fish/fishStartup';
+
+suite('Fish Startup', () => {
+    let originalXdgConfigHome: string | undefined;
+
+    setup(() => {
+        originalXdgConfigHome = process.env.XDG_CONFIG_HOME;
+    });
+
+    teardown(() => {
+        if (originalXdgConfigHome === undefined) {
+            delete process.env.XDG_CONFIG_HOME;
+        } else {
+            process.env.XDG_CONFIG_HOME = originalXdgConfigHome;
+        }
+        sinon.restore();
+    });
+
+    test('getFishProfile uses XDG_CONFIG_HOME when set', async () => {
+        const xdgConfigHome = path.join('test', 'xdg');
+        process.env.XDG_CONFIG_HOME = xdgConfigHome;
+
+        const profilePath = await getFishProfile();
+
+        assert.strictEqual(profilePath, path.join(xdgConfigHome, 'fish', 'config.fish'));
+    });
+
+    test('getFishProfile falls back to ~/.config when XDG_CONFIG_HOME is empty', async () => {
+        process.env.XDG_CONFIG_HOME = '   ';
+        const homeDir = os.homedir();
+
+        const profilePath = await getFishProfile();
+
+        assert.strictEqual(profilePath, path.join(homeDir, '.config', 'fish', 'config.fish'));
+    });
+});
