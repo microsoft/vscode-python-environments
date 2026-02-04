2fb499e45f82afc786fb10ec22ee7491013d4be9
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2026-02-04 12:34:19 -0800

    bug: terminal triggered package refresh to use correct env

--- src/managers/builtin/main.ts
+++ src/managers/builtin/main.ts
@@ -1,12 +1,10 @@
 import { Disposable, LogOutputChannel } from 'vscode';
 import { PythonEnvironmentApi } from '../../api';
 import { createSimpleDebounce } from '../../common/utils/debounce';
-import { onDidEndTerminalShellExecution } from '../../common/window.apis';
 import { createFileSystemWatcher, onDidDeleteFiles } from '../../common/workspace.apis';
 import { getPythonApi } from '../../features/pythonApi';
 import { NativePythonFinder } from '../common/nativePythonFinder';
 import { PipPackageManager } from './pipManager';
-import { isPipInstallCommand } from './pipUtils';
 import { SysPythonManager } from './sysPythonManager';
 import { VenvManager } from './venvManager';
 
@@ -40,18 +38,6 @@ export async function registerSystemPythonFeatures(
         }),
         onDidDeleteFiles(() => {
             venvDebouncedRefresh.trigger();
-        }),
-    );
-
-    disposables.push(
-        onDidEndTerminalShellExecution(async (e) => {
-            const cwd = e.terminal.shellIntegration?.cwd;
-            if (isPipInstallCommand(e.execution.commandLine.value) && cwd) {
-                const env = await venvManager.get(cwd);
-                if (env) {
-                    await pkgManager.refresh(env);
-                }
-            }
         }),
     );
 }
