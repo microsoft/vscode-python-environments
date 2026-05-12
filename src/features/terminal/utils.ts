31942cb5a2e34b9ca3d7e92fc9c6a4bd192ffe7e
Eduardo Villalpando Mello <eduardovil@microsoft.com>
2026-05-12 16:11:08 -0700

    Skip terminal if doesnt have a shell path

--- src/features/terminal/utils.ts
+++ src/features/terminal/utils.ts
@@ -395,6 +395,7 @@ export function removeAnsiEscapeCodes(str: string): string {
 export function shouldSkipTerminalActivation(terminal: Terminal): boolean {
     return (
         !!(terminal.creationOptions as TerminalOptions)?.hideFromUser ||
-        !!(terminal.creationOptions as ExtensionTerminalOptions)?.pty
+        !!(terminal.creationOptions as ExtensionTerminalOptions)?.pty ||
+        !(terminal.creationOptions as TerminalOptions)?.shellPath
     );
 }
