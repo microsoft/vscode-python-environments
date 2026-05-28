33026888f41061dffd1556be059950b2ad0be855
Eduardo Villalpando Mello <eduardovil@microsoft.com>
2026-05-27 17:22:42 -0700

    Condense fetchPackages and getPakages methods into getPackages

--- src/managers/conda/condaPackageManager.ts
+++ src/managers/conda/condaPackageManager.ts
@@ -156,38 +156,6 @@ export class CondaPackageManager implements PackageManager, Disposable {
         this.packages.clear();
     }
 
-    async fetchPackages(environment: PythonEnvironment): Promise<Package[]> {
-        const args = ['list', '-p', environment.environmentPath.fsPath, '--json'];
-        const data = await runCondaExecutable(args);
-
-        let condaPackages: { name: string; version: string }[];
-        try {
-            condaPackages = JSON.parse(data) as { name: string; version: string }[];
-        } catch (e) {
-            traceError(`Failed to parse conda list JSON output: ${data}`, e);
-            return [];
-        }
-
-        const packages: Package[] = [];
-        for (const condaPkg of condaPackages) {
-            if (condaPkg.name && condaPkg.version) {
-                packages.push(
-                    this.api.createPackageItem(
-                        {
-                            name: condaPkg.name,
-                            displayName: condaPkg.name,
-                            version: condaPkg.version,
-                            description: condaPkg.version,
-                        },
-                        environment,
-                        this,
-                    ),
-                );
-            }
-        }
-        return packages;
-    }
-
     setPackages(
         environment: PythonEnvironment,
         packages: Package[],
